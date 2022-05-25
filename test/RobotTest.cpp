ae7dfd22f9c9367c72bb002ab53caef70042097b
donfiguerres <lyndonfiguerres@gmail.com>
2022-05-25 20:18:15 +0800

    Added more test.

--- test/RobotTest.cpp
+++ test/RobotTest.cpp
@@ -19,6 +19,40 @@ TEST(RobotTest, MoveNorth) {
     EXPECT_EQ(newPosition.direction, Position::Direction::NORTH);
 }
 
+
+TEST(RobotTest, AvoidFalling) {
+    Robot robot;
+
+    Command cmd1(Command::PLACE);
+    cmd1.position = Position(5, 0, Position::Direction::NORTH);
+    robot.perform(cmd1);
+
+    Command cmd2(Command::MOVE);
+    robot.perform(cmd2);
+
+    Position newPosition = robot.getPosition();
+    EXPECT_EQ(newPosition.x, 5);
+    EXPECT_EQ(newPosition.y, 0);
+    EXPECT_EQ(newPosition.direction, Position::Direction::NORTH);
+}
+
+
+TEST(RobotTest, Rotate) {
+    Robot robot;
+
+    Command cmd1(Command::PLACE);
+    cmd1.position = Position(0, 0, Position::Direction::NORTH);
+    robot.perform(cmd1);
+
+    Command cmd2(Command::RIGHT);
+    robot.perform(cmd2);
+
+    Position newPosition = robot.getPosition();
+    EXPECT_EQ(newPosition.x, 0);
+    EXPECT_EQ(newPosition.y, 0);
+    EXPECT_EQ(newPosition.direction, Position::Direction::EAST);
+}
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
